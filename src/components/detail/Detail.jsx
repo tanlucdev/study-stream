c9d779653700828d7501138f90a60226a20e388e
tanlucdev <tanluc.dev@gmail.com>
2024-04-27 21:26:19 +0700

    Design chat message.

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -3,6 +3,58 @@ import './detail.scss'
 
 export default function Detail() {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Kento</h2>
+        <p>Lorem ipsum, dolor sit.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://images.pexels.com/photos/5079604/pexels-photo-5079604.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                <span>Photo_2024.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon' />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://images.pexels.com/photos/5079604/pexels-photo-5079604.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                <span>Photo_2024.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon' />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+        <button className='logout'>Log out</button>
+
+      </div>
+      <div className="user"></div>
+    </div>
   )
 }
