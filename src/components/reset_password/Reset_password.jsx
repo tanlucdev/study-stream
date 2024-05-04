f63e48cd0ee0ea2c9db5c59381bf23d6636f1ef6
tanlucdev <tanluc.dev@gmail.com>
2024-05-04 15:55:13 +0700

    Done validate email

--- src/components/reset_password/Reset_password.jsx
+++ src/components/reset_password/Reset_password.jsx
@@ -1,20 +1,43 @@
 import React from 'react'
 import { sendPasswordResetEmail } from 'firebase/auth'
+import { collection, getDocs } from 'firebase/firestore'
 import { database } from '../../lib/firebase'
 import { toast } from 'react-toastify'
 import Notification from '../notification/Notification'
+import { db } from '../../lib/firebase'
 
 export default function Reset_password() {
 
+  const checkEmailRegistered = async (email) => {
+    // Lấy tất cả người dùng
+    const usersRef = collection(db, 'users')
+    const querySnapshot = await getDocs(usersRef)
+
+    // Tìm kiếm trong mảng các email
+    const foundUser = querySnapshot.docs.find(doc => {
+      const userData = doc.data()
+      const userEmail = userData.email
+      return userEmail === email
+    })
+    // Nếu email được tìm thấy, trả về true; ngược lại, trả về false
+    return !!foundUser
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const emailVal = e.target.email.value;
-    console.log("email value:", emailVal)
-    sendPasswordResetEmail(database, emailVal).then(data => {
-      toast.success("Check your email!")
-    }).catch(err => {
-      toast.error(err.code)
-    })
+    const emailVal = e.target.email.value
+    console.log(">>>> emailVal: ", emailVal)
+    const emailRegistered = await checkEmailRegistered(emailVal)
+    console.log("emailRegistered: ", emailRegistered)
+    if (!emailRegistered) {
+      toast.error("Email is not registered.")
+    } else {
+      sendPasswordResetEmail(database, emailVal).then(data => {
+        toast.success("Check your email!")
+      }).catch(err => {
+        toast.error(err.code)
+      })
+    }
   }
 
   return (
